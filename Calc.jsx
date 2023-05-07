656fe078f23e6ad0765337e5803ba5c1a78857db
Shilpa3107 <116442351+Shilpa3107@users.noreply.github.com>
2023-05-08 02:28:58 +0530

    Creating Simple calculator

---
+++ Calc.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function add(x,y){
+    return (x+y);
+}
+function sub(x,y){
+    return (x-y);
+}
+function div(x,y){
+    return (x/y);
+}
+function mult(x,y){
+    return (x*y);
+}
+ export {add,sub,div,mult};
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import Recat from 'react';
-import ReactDOM from'react-dom';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {add,sub,div,mult} from './Calc';
 
-ReactDOM.render(<App/>, document.getElementById("root")
+ReactDOM.render(
+<>
+<ul>
+    <li>Sum of two no is {add(40,4)}</li>
+    <li>Sub of two no is {sub(30,3)}</li>
+    <li>Div of two no is {div(10,3)}</li>
+    <li>Mult of two no is {mult(10,3)}</li>
+</ul>
+
+</>,
+document.getElementById('root')
 );
