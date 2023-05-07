af74b248cff58ff7798d050e0b967b60fd2dd338
Shilpa3107 <116442351+Shilpa3107@users.noreply.github.com>
2023-05-07 23:18:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Heading from './Heading';
+import Para from './Para';
+import Table from './Table';
+
+function App(){
+    return (
+        <>
+        <Heading/>
+        <Para/>
+        <Table/>
+        </>
+    );
+}
+export default App;
---
+++ index1.js
@@ -0,0 +1,6 @@
+import Recat from 'react';
+import ReactDOM from'react-dom';
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById("root")
+);
