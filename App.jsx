b0fcda9f3efb00b6e7de7df3790e4533e3ad9ce8
Shilpa3107 <116442351+Shilpa3107@users.noreply.github.com>
2023-05-10 06:16:53 +0530

    Using ternary operator

--- App.jsx
+++ App.jsx
@@ -6,31 +6,21 @@ import Amazon from './Amazon';
 
 const favpro = "NETFLIX";
 
-const Favprog = () =>{
-    if(favpro === "NETFLIX"){
-       return <Netflix/>;
-    }
-    else {
-        return <Amazon/>;
-    }
-};
+// const Favprog = () =>{
+//     if(favpro === "NETFLIX"){
+//        return <Netflix/>;
+//     }
+//     else {
+//         return <Amazon/>;
+//     }
+// };
 
 function App(){
     return(
         <>
         <h1 className = "heading">LIST OF 5 WEB SERIES ON NETFLIX</h1>
-         {/* {Data.map((val) => {
-            return(
-        <Card 
-        key = {val.id}
-        imgsrc = {val.imgsrc}
-        sr = {val.sr}
-        sernam = {val.sernam}
-        link = {val.link}
-        />
-    );
-         }) } */}
-         <Favprog/>
+        
+         {favpro === "NETFLIX" ? <Netflix/> : <Amazon/>};
     </>
     );
 }
